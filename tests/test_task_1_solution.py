91894f33010b1b3405406a1c7f6f48973ffa58c8
Alexander Petrovskiy <uiautomation@hotmail.com>
2022-03-23 08:46:01 +0300

    Feature/3 task 1 (#3)
    * the 3rd version of task 1 solution
    
    * the last fixes

--- tests/test_task_1_solution.py
+++ tests/test_task_1_solution.py
@@ -1,141 +1,201 @@
 from typing import Any, List
 import pytest
 
-from src.task_1_solution import task_1_desired_places, task_1_desired_places_new
+from src.task_1_solution import task_1_desired_places
 
-TEST_DATA = [([
-                  {
-                      'school': True,
-                      'shop': True,
-                      'gym': False,
-                      'restourant': True
-                  },
-                  {
-                      'school': True,
-                      'shop': False,
-                      'gym': False,
-                      'restourant': True
-                  },
-                  {
-                      'school': False,
-                      'shop': False,
-                      'gym': False,
-                      'restourant': True
-                  },
-                  {
-                      'school': True,
-                      'shop': False,
-                      'gym': False,
-                      'restourant': False
-                  },
-                  {
-                      'school': False,
-                      'shop': False,
-                      'gym': True,
-                      'restourant': False
-                  }
-              ], ['school', 'shop', 'gym'], 0),
-    ([
-         {
-             'school': True,
-             'shop': True,
-             'gym': False,
-             'restourant': True
-         },
-         {
-             'school': True,
-             'shop': False,
-             'gym': False,
-             'restourant': True
-         },
-         {
-             'school': False,
-             'shop': False,
-             'gym': False,
-             'restourant': True
-         },
-         {
-             'school': True,
-             'shop': False,
-             'gym': False,
-             'restourant': False
-         },
-         {
-             'school': False,
-             'shop': False,
-             'gym': True,
-             'restourant': False
-         }
-     ], ['gym'], 4),
-    ([
-         {
-             'school': True,
-             'shop': True,
-             'gym': False,
-             'restourant': True
-         },
-         {
-             'school': True,
-             'shop': False,
-             'gym': False,
-             'restourant': True
-         },
-         {
-             'school': False,
-             'shop': False,
-             'gym': False,
-             'restourant': True
-         },
-         {
-             'school': True,
-             'shop': False,
-             'gym': False,
-             'restourant': False
-         },
-         {
-             'school': False,
-             'shop': False,
-             'gym': True,
-             'restourant': False
-         }
-     ], ['shop', 'gym'], 0), ([
-                                  {
-                                      'school': True,
-                                      'shop': True,
-                                      'gym': False,
-                                      'restourant': True
-                                  },
-                                  {
-                                      'school': True,
-                                      'shop': False,
-                                      'gym': False,
-                                      'restourant': True
-                                  },
-                                  {
-                                      'school': False,
-                                      'shop': False,
-                                      'gym': False,
-                                      'restourant': True
-                                  },
-                                  {
-                                      'school': True,
-                                      'shop': False,
-                                      'gym': False,
-                                      'restourant': False
-                                  },
-                                  {
-                                      'school': False,
-                                      'shop': False,
-                                      'gym': True,
-                                      'restourant': False
-                                  }
-                              ], ['school', 'shop'], 0)
-]
+TEST_DATA = [("original sample", [{
+    'school': True,
+    'shop': True,
+    'gym': False,
+    'restourant': True
+}, {
+    'school': True,
+    'shop': False,
+    'gym': False,
+    'restourant': True
+}, {
+    'school': False,
+    'shop': False,
+    'gym': False,
+    'restourant': True
+}, {
+    'school': True,
+    'shop': False,
+    'gym': False,
+    'restourant': False
+}, {
+    'school': False,
+    'shop': False,
+    'gym': True,
+    'restourant': False
+}], ['school', 'shop', 'gym'], 0),
+             ("the last block", [{
+                 'school': True,
+                 'shop': True,
+                 'gym': False,
+                 'restourant': True
+             }, {
+                 'school': True,
+                 'shop': False,
+                 'gym': False,
+                 'restourant': True
+             }, {
+                 'school': False,
+                 'shop': False,
+                 'gym': False,
+                 'restourant': True
+             }, {
+                 'school': True,
+                 'shop': False,
+                 'gym': False,
+                 'restourant': False
+             }, {
+                 'school': False,
+                 'shop': False,
+                 'gym': True,
+                 'restourant': False
+             }], ['gym'], 4),
+             ("the first block of the two possible", [{
+                 'school': True,
+                 'shop': True,
+                 'gym': False,
+                 'restourant': True
+             }, {
+                 'school': True,
+                 'shop': False,
+                 'gym': False,
+                 'restourant': True
+             }, {
+                 'school': False,
+                 'shop': False,
+                 'gym': False,
+                 'restourant': True
+             }, {
+                 'school': True,
+                 'shop': False,
+                 'gym': False,
+                 'restourant': False
+             }, {
+                 'school': False,
+                 'shop': False,
+                 'gym': True,
+                 'restourant': False
+             }], ['shop', 'gym'], 0),
+             ("the best choice is obvious", [{
+                 'school': True,
+                 'shop': True,
+                 'gym': False,
+                 'restourant': True
+             }, {
+                 'school': True,
+                 'shop': False,
+                 'gym': False,
+                 'restourant': True
+             }, {
+                 'school': False,
+                 'shop': False,
+                 'gym': False,
+                 'restourant': True
+             }, {
+                 'school': True,
+                 'shop': False,
+                 'gym': False,
+                 'restourant': False
+             }, {
+                 'school': False,
+                 'shop': False,
+                 'gym': True,
+                 'restourant': False
+             }], ['school', 'shop'], 0),
+             ("two fields' list with the best choice", [{
+                 'a': True,
+                 'b': False
+             }, {
+                 'a': False,
+                 'b': True
+             }, {
+                 'a': True,
+                 'b': False
+             }, {
+                 'a': True,
+                 'b': True
+             }, {
+                 'a': True,
+                 'b': False
+             }, {
+                 'a': True,
+                 'b': False
+             }, {
+                 'a': True,
+                 'b': False
+             }, {
+                 'a': True,
+                 'b': False
+             }], ['a', 'b'], 3),
+             ("two fields' list without the best choice", [{
+                 'a': False,
+                 'b': False
+             }, {
+                 'a': False,
+                 'b': True
+             }, {
+                 'a': True,
+                 'b': False
+             }, {
+                 'a': False,
+                 'b': False
+             }, {
+                 'a': True,
+                 'b': False
+             }, {
+                 'a': False,
+                 'b': False
+             }, {
+                 'a': False,
+                 'b': False
+             }, {
+                 'a': True,
+                 'b': False
+             }], ['a', 'b'], 1),
+             ("two fields' list with poor blocks", [{
+                 'a': False,
+                 'b': False
+             }, {
+                 'a': False,
+                 'b': False
+             }, {
+                 'a': True,
+                 'b': False
+             }, {
+                 'a': False,
+                 'b': False
+             }, {
+                 'a': False,
+                 'b': False
+             }, {
+                 'a': False,
+                 'b': False
+             }, {
+                 'a': False,
+                 'b': False
+             }, {
+                 'a': False,
+                 'b': False
+             }, {
+                 'a': True,
+                 'b': False
+             }], ['a', 'b'], 2), ("an empty list", [], ['a', 'b'], 1)]
 
 
-@pytest.mark.parametrize("blocks,desired_places,expected_house_number", TEST_DATA)
-def test_task_1_desired_places_new(blocks: List[Any], desired_places: List[str], expected_house_number: int):
-    actual_house = task_1_desired_places_new(blocks, desired_places)
+@pytest.mark.parametrize(
+    "description,blocks,desired_places,expected_house_number", TEST_DATA)
+def test_task_1_desired_places(description: str, blocks: List[Any],
+                               desired_places: List[str],
+                               expected_house_number: int):
+    actual_house = task_1_desired_places(blocks, desired_places)
 
-    assert actual_house == blocks[
-        expected_house_number], f"Wrong house, expected = {blocks[expected_house_number]}, actual = {actual_house}"
+    if expected_house_number < len(blocks):
+        assert actual_house == blocks[
+            expected_house_number], f"Wrong house, expected = {blocks[expected_house_number]}, actual = {actual_house}"
+    else:
+        assert True, "There should be an empty result"
