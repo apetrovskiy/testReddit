76a9b02b693bad987a1eee75015c664415347011
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2022-03-23 07:35:48 +0300

    experimental changes

--- src/task_1_solution.py
+++ src/task_1_solution.py
@@ -2,7 +2,7 @@ import json
 from typing import Any, List
 
 
-def task_1_desired_places(blocks: List[Any]) -> int:
+def task_1_desired_places(blocks: List[Any], desired_places: List[str]) -> int:
     """Returns a house with optimum conditions"""
     desired_house_number = -1
 
@@ -26,7 +26,8 @@ def task_1_desired_places(blocks: List[Any]) -> int:
         last_shop = i if house[SHOP] else last_shop
         house_score[GYM] = 0 if house[GYM] else i - last_gym
         last_gym = i if house[GYM] else last_gym
-        house_score[RESTAURANT] = 0 if house[RESTAURANT] else i - last_restaurant
+        house_score[
+            RESTAURANT] = 0 if house[RESTAURANT] else i - last_restaurant
         last_restaurant = i if house[RESTAURANT] else last_restaurant
 
         house_scores.append(house_score)
@@ -34,7 +35,46 @@ def task_1_desired_places(blocks: List[Any]) -> int:
     print(house_scores)
 
     # getting the best choice - with minimum scores
-    # for i in range(len(house_scores)):
+    efficiency_of_houses = {}
+    for house_score in house_scores:
+        efficiency_of_house = {'id': house_score['id']}
+        print(efficiency_of_house)
+        efficiency = 0
+        for place in desired_places:
+            efficiency += house_score[place]
+            # efficiency_of_house = {id:house_score.id, efficiency = }
+        efficiency_of_house['score'] = efficiency
+        print(efficiency_of_house)
+        efficiency_of_houses[
+            efficiency_of_house['id']] = efficiency_of_house['score']
+    # best_choice = {(key, val) for key, val in efficiency_of_houses}
+    best_choice = min(efficiency_of_houses, key=efficiency_of_houses.get)
+
+    print(efficiency_of_houses)
+    print(best_choice)
+
+    # return blocks[desired_house_number]
+    return blocks[best_choice]
 
 
-    return blocks[desired_house_number]
+def task_1_desired_places_new(blocks: List[Any],
+                              desired_places: List[str]) -> int:
+    """Returns a house with optimum conditions"""
+    best_choice = -1
+
+    block_scores = {}
+    for i in range(len(blocks)):
+        current_block = blocks[i]
+        block_score = {'id': i}
+        overall_block_score = 0
+        for place in desired_places:
+            if current_block[place]:
+                overall_block_score += 2
+            if not blocks[i][place] and i > 0:
+                block_scores[i - 1]['score'] += 1
+        block_score['score'] = overall_block_score
+        block_scores[block_score['id']]=block_score['score']
+    
+    best_choice=min(block_scores, key=block_scores.get())
+
+    return blocks[best_choice]
--- tests/test_task_1_solution.py
+++ tests/test_task_1_solution.py
@@ -1,7 +1,7 @@
 from typing import Any, List
 import pytest
 
-from src.task_1_solution import task_1_desired_places
+from src.task_1_solution import task_1_desired_places, task_1_desired_places_new
 
 TEST_DATA = [([
                   {
@@ -98,13 +98,44 @@ TEST_DATA = [([
              'gym': True,
              'restourant': False
          }
-     ], ['shop', 'gym'], 0)
+     ], ['shop', 'gym'], 0), ([
+                                  {
+                                      'school': True,
+                                      'shop': True,
+                                      'gym': False,
+                                      'restourant': True
+                                  },
+                                  {
+                                      'school': True,
+                                      'shop': False,
+                                      'gym': False,
+                                      'restourant': True
+                                  },
+                                  {
+                                      'school': False,
+                                      'shop': False,
+                                      'gym': False,
+                                      'restourant': True
+                                  },
+                                  {
+                                      'school': True,
+                                      'shop': False,
+                                      'gym': False,
+                                      'restourant': False
+                                  },
+                                  {
+                                      'school': False,
+                                      'shop': False,
+                                      'gym': True,
+                                      'restourant': False
+                                  }
+                              ], ['school', 'shop'], 0)
 ]
 
 
 @pytest.mark.parametrize("blocks,desired_places,expected_house_number", TEST_DATA)
-def test_task_1_desired_places(blocks: List[Any], desired_places: List[str], expected_house_number: int):
-    actual_house = task_1_desired_places(blocks)
+def test_task_1_desired_places_new(blocks: List[Any], desired_places: List[str], expected_house_number: int):
+    actual_house = task_1_desired_places_new(blocks, desired_places)
 
     assert actual_house == blocks[
         expected_house_number], f"Wrong house, expected = {blocks[expected_house_number]}, actual = {actual_house}"
